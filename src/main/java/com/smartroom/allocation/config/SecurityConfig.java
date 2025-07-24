1f313176fdf1308edad50fe65eb35d90e3f112ca
Jesse Chomba <Jessechomba2@gmail.com>
2025-07-24 10:46:57 +0300

    Allowed vercel front-end access to the backend

--- src/main/java/com/smartroom/allocation/config/SecurityConfig.java
+++ src/main/java/com/smartroom/allocation/config/SecurityConfig.java
@@ -41,7 +41,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // allow your frontend origin
+        config.setAllowedOrigins(List.of("http://localhost:3000","https://eclectis-smartroom-allocation.vercel.app")); // allow your frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true); // Optional: allow cookies/auth headers if needed
